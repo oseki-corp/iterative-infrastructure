74cac047b62f6484e227886b6dc1748752c49cc3
Emmanuel Roullit <eroullit@github.com>
2022-09-21 13:24:03 +0200

    Update variables.tf

--- terraform/github/variables.tf
+++ terraform/github/variables.tf
@@ -2,7 +2,7 @@ variable "organization_admins" {
   type        = list(string)
   description = "GitHub Organization Admins"
   default = [
-    "martedesco",  # Marcio
-    "eroullit",    # Emmanuel
+    "martedesco", # Marcio
+    "eroullit",   # Emmanuel
   ]
 }
